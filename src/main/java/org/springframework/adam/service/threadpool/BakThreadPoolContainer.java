fd53829a0eaf6ce91d0b3388a5eb6efa6d4a977a
xiaorui.ni <xiaorui.ni@vipshop.com>
2018-12-12 20:59:53 +0800

    prestartAllCoreThreads

--- src/main/java/org/springframework/adam/service/threadpool/BakThreadPoolContainer.java
+++ src/main/java/org/springframework/adam/service/threadpool/BakThreadPoolContainer.java
@@ -4,9 +4,13 @@
 package org.springframework.adam.service.threadpool;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -15,21 +19,28 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class BakThreadPoolContainer {
 
-	private final static Executor finalBakThreadPool = Executors.newFixedThreadPool(256, new ThreadFactory() {
-		private final AtomicInteger threadNumber = new AtomicInteger(1);
-
-		@Override
-		public Thread newThread(Runnable r) {
-			Thread t = new Thread(Thread.currentThread().getThreadGroup(), r, "adam backup thread :" + threadNumber.getAndIncrement(), 0);
-			if (t.isDaemon())
-				t.setDaemon(false);
-			if (t.getPriority() != Thread.NORM_PRIORITY)
-				t.setPriority(Thread.NORM_PRIORITY);
-			return t;
-		}
-	});
+	private final static Executor finalBakThreadPool = getFinalBakThreadPool();
 
 	private static Executor bakThreadPool;
+
+	private static Executor getFinalBakThreadPool() {
+
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(256, 256, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), new ThreadFactory() {
+			private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+			@Override
+			public Thread newThread(Runnable r) {
+				Thread t = new Thread(Thread.currentThread().getThreadGroup(), r, "adam backup thread :" + threadNumber.getAndIncrement(), 0);
+				if (t.isDaemon())
+					t.setDaemon(false);
+				if (t.getPriority() != Thread.NORM_PRIORITY)
+					t.setPriority(Thread.NORM_PRIORITY);
+				return t;
+			}
+		});
+		executor.prestartAllCoreThreads();
+		return executor;
+	}
 
 	/**
 	 * @return the bakThreadPool
