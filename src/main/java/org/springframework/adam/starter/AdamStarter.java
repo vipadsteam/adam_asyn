861102243ed4fe85825c936829c6d58c2d11e707
nixiaorui <nixiaorui@172.19.9.232>
2019-10-15 10:22:07 +0800

    support spring starter

---
+++ src/main/java/org/springframework/adam/starter/AdamStarter.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.springframework.adam.starter;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author nixiaorui
+ *
+ */
+@Configuration
+@ComponentScan("org.springframework.adam")
+public class AdamStarter {
+
+}
